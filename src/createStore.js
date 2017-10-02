85f73be256f57be5ccde196bfa46ad03204852d2
Oliver Spring <oliver9589@gmail.com>
2017-10-02 18:04:00 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,26 @@
-export default function createStore(reducer){
-// your code here!
+import countReducer from './reducers/countReducer';
 
+export default function createStore(reducer){
+  let state;
+  let listeners = [];
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach (listener => listener())
+  }
+
+  dispatch({})
+
+  const getState = () => {
+    return state;
+  }
+  const subscribe = (listener) => {
+    listeners.push(listener)
+  }
+
+  return {
+    getState: getState,
+    dispatch: dispatch,
+    subscribe: subscribe
+  }
 }
