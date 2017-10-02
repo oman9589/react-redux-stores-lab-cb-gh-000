9664f8f550b83ea6bbba4d12677c64992b9a4965
Oliver Spring <oliver9589@gmail.com>
2017-10-02 17:55:06 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,18 @@
 export default function createStore(reducer){
-// your code here!
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach (listener => listener())
+  }
 
+  dispatch({})
+
+  return {
+    getState: getState,
+    dispatch: dispatch,
+  }
 }
+
+const store = createStore(countReducer)
+store.getState()
+store.dispatch({type: 'INCREMENT_COUNT'})
+store.getState()
