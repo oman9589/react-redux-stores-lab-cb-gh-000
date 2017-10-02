ef5a668b43f8f892992085feccab239527747066
Oliver Spring <oliver9589@gmail.com>
2017-10-02 17:57:15 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,10 +1,16 @@
 export default function createStore(reducer){
+  let state;
+  
   const dispatch = (action) => {
     state = reducer(state, action)
     listeners.forEach (listener => listener())
   }
 
   dispatch({})
+
+  const getState = () => {
+    return state
+  }
 
   return {
     getState: getState,
